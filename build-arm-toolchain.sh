affc2d3340f425b6f78e6912704e29057d55de5f
Knut Welzel <knut.welzel@googlemail.com>
2014-09-01 11:56:15 +0200

    Initial source of toolchain 13.10

--- build-arm-toolchain.sh
+++ build-arm-toolchain.sh
@@ -19,8 +19,21 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # 
 
+
 # Base path, wher this script is located 
 BASEPATH=$(pwd)
+
+
+# maximum number of open file descriptors to 1024
+ulimit -n 1024
+
+
+# Exit script if not Max OS X
+if [ "$(uname -s)" != "Darwin" ]; then
+	echo "This script is designed for Mac OS X only!"
+	exit 0
+fi
+
 
 # include global variables
 source ./inc/global.cfg
@@ -33,37 +46,52 @@ source ./inc/download.sh
 # Include build functions
 source ./inc/build.sh
 
-# maximum number of open file descriptors to 1024
-ulimit -n 1024
 
 # Set Enviroment
 export TARGET=${glb_target}
 export PREFIX=${glb_prefix}
 export PATH=/usr/local/bin:$PREFIX/bin:$PATH
 
+BUILD=`gcc -v 2>&1 | grep "Target" | sed 's/Target:[ /t]*//'`
+BUILDVERSION="Illuminux Mac OSX Intelx86 based on Linaro ${glb_linaro_version}"
+BUGURL="https://github.com/Illuminux/arm-cross-toolchain-Mac_OS_X/issues"
+JOBS="-j`sysctl hw.ncpu | awk '{print $2}'`"
+
+
+
 # Clear screen
 clear 
 
-if [ "$(uname -s)" != "Darwin" ]; then
-	echo "This script is designed for Mac OS X only!"
-	exit 0
-fi
-
-echo "This script will build ARM Linux Cross-Toolchain on and for Mac" \
-	 "OS X, based on Linaro Toolchain Sources."
-echo "During the execution several files and programs will be downloaded" \
-	 "from the Internet and installed on your Computer."
-echo "For the execution 10 GB of free hard drives memory will be needed!"
-
 while true; do
-	read -p "Continue the script? [Y/n] " Yn
+
+	echo "This script will build ARM Linux Cross-Toolchain on and for Mac OS X,"
+	echo "based on Linaro Toolchain Sources."
+	echo "During the execution, several files and programs will be downloaded" 
+	echo "from the Internet and installed on your Computer."
+	echo "For the execution ${glb_disk_image_size}Bytes of free hard drives"
+	echo "memory will be needed!"
+	echo
+	echo "This program comes with ABSOLUTELY NO WARRANTY; for details type [l]."
+	echo "This is free software, and you are welcome to redistribute it"
+	echo "under certain conditions; type [l] for details."
+	echo
+	
+	read -p "Continue the script [Y/n]? Or for read the License [l]: " Yn
 	Yn=${Yn:-Y}
 	case $Yn in
 		[Yy]* ) break;;
+		[Ll]* ) less gpl-3.0.txt; clear;;
 		[Nn]* ) clear; exit 0;;
-			* ) echo "Please answer C (Continue) or a (abort).";;
+			* ) clear; echo  "Please answer Y (Yes) or n (No)."; echo;;
 	esac
 done
+
+clear
+echo "Start build process. This may take several hours!"
+echo
+
+# Create directories
+create_dir_structure
 
 # Check if command line tools are installed. If not, installe them.
 check_for_Command_Line_Tools
@@ -79,27 +107,69 @@ create_image
 # get Linaro crosscompiler sources 
 download_sources
 
-# Build Linux Kernel Source and Headers
-build_kernel
+# Build sysroot
+build_sysroot
+
+# Build Binutils
+build_gmp
+
+# Build mpfr
+build_mpfr
+
+# Build isl
+build_isl
+
+# Build cloog
+build_cloog
+
+# Build mpc
+build_mpc
+
+# Build zlib
+build_zlib
 
 # Build Binutils
 build_binutils
 
-# Build GCC Part 1
+# Build GCC Part 1 static core C compiler
 build_gcc1
 
-# Build Embedded GLIBC
-build_eglibc
+# Build Linux Kernel Source and Headers
+build_kernel
 
-# Build gcc part 2
+# Rebuild sysroot
+build_sysroot
+
+# Build gcc part 2 shared core C compiler
 build_gcc2
+
+# Build Embedded GLIBC
+#build_eglibc
+
+# Build gcc part 3
+build_gcc3
+
+# Build ncurses
+build_ncurses
+
+# Build eXpat
+build_expat
+
+# Build gdb 
+build_gdb
+
+# Build pkgconf
+build_pkgconf
 
 # remove temporrery links
 package_manager_dellinks
+
+# Build Embedded GLIBC
+#build_eglibc
 
 finish_build
 
 echo ""
 echo "ARM Linux Cross-Toolchain for Mac OS X was build successfully."
-echo "You can find them in '/usr/local/arm-linux-hf'."
+echo "You can find them in '${glb_build_path}'."
 echo ""
--- inc/global.sh
+++ inc/global.sh
@@ -1,70 +1,115 @@
 #!/bin/bash
 
-
+##
 ## Create an case senitive disk image and mount it to /Volumes/[ImageName]
-## @autor Knut
+## 
 create_image(){
 	
-	size="10M"
-
-	echo -n "Create case sensitive image" 2>&1 | tee -a ${glb_log_build}
-	if [ ! -f "${glb_disk_image_name}.dmg" ]; then
-		hdiutil create "${glb_disk_image_name}.dmg" -fs "Case-sensitive Journaled HFS+" -size ${glb_disk_image_size} -volname ${glb_disk_image_name} || exit 1
+	# Create image if not exists 
+	echo "Create case sensitive image" 2>&1 | tee -a $glb_build_log 
+	if [ ! -f "${glb_disk_image_name}.sparseimage" ]; then
+		hdiutil create "${glb_disk_image_name}.sparseimage" \
+			-type SPARSE \
+			-fs JHFS+X \
+			-size ${glb_disk_image_size} \
+			-volname ${glb_disk_image_name} || exit 1
 	else
-		echo "already exists" 2>&1 | tee -a ${glb_log_build}
+		echo "already exists" 2>&1 | tee -a $glb_build_log
 	fi
 	
-	echo -n "Mounting image... " 2>&1 | tee -a ${glb_log_build}
+	# Mount image
+	echo -n "Mounting image... " 2>&1 | tee -a $glb_build_log
 	if [ ! -d "${glb_disk_image_path}" ]; then 
-		hdiutil attach "${glb_disk_image_name}.dmg" >/dev/null 2>&1 || exit 1
-		echo "mounted to ${glb_disk_image_path}" 2>&1 | tee -a ${glb_log_build}
+		hdiutil attach ${glb_disk_image_name}.sparseimage -mountroot $BASEPATH >/dev/null 2>&1 || exit 1
+		echo "mounted to ${glb_disk_image_path}" 2>&1 | tee -a $glb_build_log
 	else
-		echo "already mounted to ${glb_disk_image_path}" 2>&1 | tee -a ${glb_log_build}
+		echo "already mounted to ${glb_disk_image_path}" 2>&1 | tee -a $glb_build_log
 	fi
 	
-	echo -n "Create download directory... " 2>&1 | tee -a ${glb_log_build}
-	if [ ! -d "${glb_download_path}" ]; then
-		mkdir ${glb_download_path} || exit 1
-		echo "done" 2>&1 | tee -a ${glb_log_build}
-	else
-		echo "already exists" 2>&1 | tee -a ${glb_log_build}
-	fi 
-	
-	echo -n "Create source directory... " 2>&1 | tee -a ${glb_log_build}
-	if [ ! -d "${glb_source_path}" ]; then
-		mkdir ${glb_source_path} || exit 1
-		echo "done" 2>&1 | tee -a ${glb_log_build}
-	else
-		echo "already exists" 2>&1 | tee -a ${glb_log_build}
-	fi 
-	
-	echo -n "Create build directory... " 2>&1 | tee -a ${glb_log_build}
-	if [ ! -d "${glb_build_path}" ]; then
-		mkdir ${glb_build_path} || exit 1
-		echo "done" 2>&1 | tee -a ${glb_log_build}
-	else
-		echo "already exists" 2>&1 | tee -a ${glb_log_build}
+	# Create "kernel-build" directory if not exist
+	echo -n "Create kernel build directory... " 2>&1 | tee -a $glb_build_log 
+	if [ -d "${glb_disk_image_path}/build" ]; then
+		rm -rf ${glb_disk_image_path}/build
 	fi
-	
-	echo -n "Create build directory... " 2>&1 | tee -a ${glb_log_build}
-	if [ ! -d "${glb_log_path}" ]; then
-		mkdir ${glb_log_path} || exit 1
-		echo "done" 2>&1 | tee -a ${glb_log_build}
-	else
-		rm -rf ${glb_log_path}/*
-		echo "already exists" 2>&1 | tee -a ${glb_log_build}
-	fi
+	mkdir ${glb_disk_image_path}/build || exit 1
+	echo "done" 2>&1 | tee -a $glb_build_log
 }
 
 
+##
+## Create directories
+## 
+create_dir_structure(){
+			
+	# Create "download" directory if not exist
+	echo -n "Create download directory... " 
+	if [ ! -d "${glb_download_path}" ]; then
+		mkdir -p ${glb_download_path} || exit 1
+		echo "done" 
+	else
+		echo "already exists" 
+	fi 
+
+	# Create "src" directory if not exist	
+	echo -n "Create source directory... " 
+	if [ ! -d "${glb_source_path}" ]; then
+		mkdir -p ${glb_source_path} || exit 1
+		echo "done" 
+	else
+		echo "already exists" 
+	fi 
+	
+	# Create "build" directory if not exist
+	echo -n "Create build directory... " 
+	if [ -d "${glb_build_path}" ]; then
+		rm -rf ${glb_build_path}
+	fi
+	mkdir -p ${glb_build_path} || exit 1
+	mkdir -p ${glb_build_path}/static || exit 1
+	mkdir -p ${glb_build_path}/gcc-core-static || exit 1
+	mkdir -p ${glb_build_path}/gcc-core-shared || exit 1
+	mkdir -p ${glb_build_path}/build-kernel-headers || exit 1
+	echo "done" 
+
+
+	# Create "install" directory if not exist	
+	echo -n "Create install directory... " 
+	if [ -d "${glb_prefix}" ]; then
+		rm -rf ${glb_prefix}
+	fi
+	mkdir -p ${glb_prefix} || exit 1
+	mkdir -p ${glb_prefix}/arm-linux-gnueabihf || exit 1
+	echo "done" 
+	
+	
+	# Create "log" directory if not exist	
+	echo -n "Create log directory... " 
+	if [ -d "${glb_log_path}" ]; then
+		rm -rf ${glb_log_path}
+	fi
+	mkdir -p ${glb_log_path} || exit 1
+	touch ${glb_build_log}
+	echo "done" 
+}
+
+
+## 
+## Check if Xcode Command Line Tools are installd.
+## If not try to install Command Line Tools
+##
 check_for_Command_Line_Tools(){
 	
-	echo -n "Checking for Command Line Tools... " 2>&1 | tee -a ${glb_log_build}
+	echo -n "Checking for Xcode Command Line Tools... " 2>&1 | tee -a $glb_build_log
+	
 	if [ ! -f "/Library/Developer/CommandLineTools/usr/bin/gcc" ]; then
-		echo "not installed" 2>&1 | tee -a ${glb_log_build}
-		echo "Command Line Tools are required for the following steps."
+		
+		echo "not installed" 2>&1 | tee -a $glb_build_log
+		echo "Command Line Tools are required for the following steps." 2>&1 | tee -a $glb_build_log
+		
 		while true; do
+		
 			read -p "Install Command Line Tools or cancel the script? [I/c] " Ic
+		
 			Ic=${Ic:-I}
 			case $Ic in
 				[Ii]* ) install_Command_Line_Tools; break;;
@@ -73,33 +118,45 @@ check_for_Command_Line_Tools(){
 			esac
 		done
 	else
-		echo "installed" 2>&1 | tee -a ${glb_log_build}
+		echo "installed" 2>&1 | tee -a $glb_build_log
 	fi
 }
 
+
+##
+## Install Xcode Command Line Tools
+## Script will be abort for this stepp
+##
 install_Command_Line_Tools(){
 
-	echo -n "Installing Command Line Tools... " 2>&1 | tee -a ${glb_log_build}
+	echo -n "Installing Command Line Tools... "
+	
+	# Install Command Line Tools
 	xcode-select --install 
+	
 	echo "Please wait until the command line tools has been installed and run the script again!"
+	
+	# Script musz be abort at this point, because the installer will not halt the script
 	exit 0
 }
 
+
+##
+## End of the build
+##
 finish_build(){
-	
-	cd ${glb_disk_image_path}
-	
-	echo -n "Create compressed archiv... " 2>&1 | tee -a ${glb_log_build}
-	mv build arm-linux
-	tar -czf "$BASEPATH/${glb_target}.tar.gz" ${glb_build_path}/ >/dev/null 2>&1 || exit 1
-	mv arm-linux build 
-	echo "done" 2>&1 | tee -a ${glb_log_build}
 	
 	cd $BASEPATH
 	
-	echo -n "Unmount build image... " 2>&1 | tee -a ${glb_log_build}
+	echo -n "Create compressed archiv... " 
+	tar -cJPf ./${glb_build_name}.tar.xz ${glb_build_path} >/dev/null 2>&1 || exit 1
+	echo "done" 
+
+	cd $BASEPATH
+
+	echo -n "Unmount build image... " 
 	hdiutil detach ${glb_disk_image_path} >/dev/null 2>&1
-	echo "done" 2>&1 | tee -a ${glb_log_build}
+	echo "done" 
 	
 	while true; do
 		read -p "Should the build image be deleted? [y/N] " yN
